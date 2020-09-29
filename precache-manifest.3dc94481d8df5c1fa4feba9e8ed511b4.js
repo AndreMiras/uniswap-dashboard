0f0165656bd5b7e0fc258c9012da316b9daaf358
Andre Miras <andre.miras@gmail.com>
2020-09-29 22:02:26 +0200

    Updates

---
+++ precache-manifest.3dc94481d8df5c1fa4feba9e8ed511b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2125951328089ba532a3d97ebc30aa7",
+    "url": "/uniswap-dashboard/index.html"
+  },
+  {
+    "revision": "9a97988f58f05ef3c5d6",
+    "url": "/uniswap-dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04832b8220f39a893c99",
+    "url": "/uniswap-dashboard/static/js/0.3433e1d1.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/uniswap-dashboard/static/js/0.3433e1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc7cec70c44753a3000",
+    "url": "/uniswap-dashboard/static/js/1.7866fad6.chunk.js"
+  },
+  {
+    "revision": "8e9f99f2f00df2a84d7a0f4e2ee8a88a",
+    "url": "/uniswap-dashboard/static/js/1.7866fad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d4d4415ed9fa4aa39e",
+    "url": "/uniswap-dashboard/static/js/2.24fe2b74.chunk.js"
+  },
+  {
+    "revision": "7c80b930486f2cce1efc",
+    "url": "/uniswap-dashboard/static/js/5.e135e746.chunk.js"
+  },
+  {
+    "revision": "91e9bfd1d08880b66e0e6b773fb8e781",
+    "url": "/uniswap-dashboard/static/js/5.e135e746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f306a344f5c546cdb55",
+    "url": "/uniswap-dashboard/static/js/6.01a55829.chunk.js"
+  },
+  {
+    "revision": "44e3503abf6efcb937ab",
+    "url": "/uniswap-dashboard/static/js/7.b3dc332a.chunk.js"
+  },
+  {
+    "revision": "9e2df5f6c7fadb6570fa",
+    "url": "/uniswap-dashboard/static/js/8.460f2b12.chunk.js"
+  },
+  {
+    "revision": "9a97988f58f05ef3c5d6",
+    "url": "/uniswap-dashboard/static/js/main.a87b9818.chunk.js"
+  },
+  {
+    "revision": "59c6232af224cbc5d2b1",
+    "url": "/uniswap-dashboard/static/js/runtime-main.8b12f387.js"
+  }
+]);
