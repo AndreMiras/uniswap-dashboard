2c697aebce9578c49da2f2502efe840b789679c8
Andre Miras <andre.miras@gmail.com>
2020-09-29 22:34:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uniswap-dashboard/precache-manifest.4eb984fea261998c2b3573d9b3fe740e.js"
+  "/uniswap-dashboard/precache-manifest.b06a1e66381598df09cbd165ab28f297.js"
 );
 
 self.addEventListener('message', (event) => {
